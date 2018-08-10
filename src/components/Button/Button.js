56132513f494bc52ed52eceb4cc7b23d5037311b
Loke Chen Lin <chenlin@seekasia.com>
2018-08-10 15:52:51 +0800

    Add new component - button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,27 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
 const Button = (props) => {
-  const { text, onPress } = props;
-  return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Text style={styles.buttonText}>
-        {text}
-      </Text>
-    </TouchableOpacity>
-  );
+    const { text, onPress } = props;
+    return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <Text style={styles.buttonText}>
+                {text}
+            </Text>
+        </TouchableOpacity>
+    );
 };
 
 Button.propTypes = {
-  text: React.PropTypes.string,
-  onPress: React.PropTypes.func,
-};
-
-Button.defaultProps = {
-  text: 'Button Text',
-  // eslint-disable-next-line no-console
-  onPress: () => console.log('Button Pressed'),
+    text: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
 };
 
 export default Button;
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,15 +2,15 @@ import { StyleSheet } from 'react-native';
 import { colors } from '../../config/styles';
 
 export default StyleSheet.create({
-  button: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    backgroundColor: colors.buttonBackground,
-    margin: 5,
-  },
-  buttonText: {
-    color: colors.buttonText,
-    fontSize: 16,
-    fontWeight: '500',
-  },
+    button: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: colors.buttonBackground,
+        margin: 5,
+    },
+    buttonText: {
+        color: colors.buttonText,
+        fontSize: 16,
+        fontWeight: '500',
+    },
 });
